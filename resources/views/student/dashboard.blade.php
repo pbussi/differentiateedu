5b21ea26cca4fed34eb466f12a4fd13b3a3ac5d5
pbussi@gmal.com <pbussi@gmal.com>
2021-06-02 08:42:48 -0300

    error en dashboard student con closed class

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -120,7 +120,7 @@
                 <div class="card-columns">
                   @foreach ($courses as $course)
                     @if ($course->status=="1")
-                      <div class="card"><a href="{{url("teacherQuestion/list/{$course->id}")}}" >
+                      <div class="card"><a href="{{url("myactivities/questionList/{$course->id}")}}">
 
                         <img class="card-img-top" src="{{url("file/download/{$course->picture->hash}")}}"alt="Card image cap" width="297px" height="180px" style="filter:grayscale(100%);">
                         <div class="card-body">
@@ -128,13 +128,9 @@
 
                             <p class="card-text">{{$course->description_heading}}</p>
                             <p class="card-text">Due date: <i>{{date('m-d-Y', strtotime($course->due_date))}}</i></p>
-                            <p class="card-text"><small class="text-muted">Total Students:</small></p>
-                            <p class="card-text"><small class="text-muted">Created At:{{$course->created_at}}</small></p>
+                            
                         </div>
-                          <div class="card-footer" style="text-align: right;">
-                                <a href="{{url("mycourses/edit/{$course->id}")}}" ><span class="label label-success label-rounded">Edit </span></a>
-                                <a href="{{url("mycourses/edit/{$course->id}")}}" ><span class="label label-warning label-rounded">Delete </span></a>
-                            </div>
+                         
                     </div>  
                     @endif 
                   @endforeach
